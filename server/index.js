f5dc87308ba1f1d1f3554a41ae088f5f29f9ca4b
mattyk0207 <mattia.lodi@gmail.com>
2023-09-12 12:04:07 +0200

    .

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const fs = require('fs');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'sdfklknwaeivow2i4ofmwp30';
 
-
+app.options('*', cors());
 //middleware
 app.use(express.json());
 app.use(cors({ credentials: true, origin: "https://sito-pane-app.vercel.app:3000"}));  //http://localhost:3000
